d59f7b50e19e40317c51eafb0cd48450086be3fe
Harnoor Singh <iharnoor@gmail.com>
2025-06-20 11:18:11 -0400

    Initial Commit

---
+++ screenshot/screenshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  screenshotApp.swift
+//  screenshot
+//
+//  Created by harnoorsingh on 6/20/25.
+//
+
+import SwiftUI
+
+@main
+struct screenshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ screenshotTests/screenshotTests.swift
@@ -0,0 +1,16 @@
+//
+//  screenshotTests.swift
+//  screenshotTests
+//
+//  Created by harnoorsingh on 6/20/25.
+//
+
+import Testing
+
+struct screenshotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
